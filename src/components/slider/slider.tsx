57d2633013ac062589d74f9f537ce8431a86539d
Valeriy Brazhnik <valerijbrazhnik@yandex.ru>
2023-12-11 05:59:56 +0300

    fix: верстка линий и круга

--- src/components/slider/slider.tsx
+++ src/components/slider/slider.tsx
@@ -2,8 +2,17 @@ import './slider.scss';
 
 export const Slider = () => {
   return (
-    <div className='slider'>
-      <div className="circle"></div>
+    <div className='slider-container'>
+      <div className='slider'>
+        <div className="circle">
+          <div className='line horizontal-line'></div>
+          <div className='line vertical-line'></div>
+        </div>
+      </div>
+
+      <div className='events-container'>
+        <div>13 сентября — частное солнечное затмение, видимое в Южной Африке и части Антарктиды</div>
+      </div>
     </div>
   );
 }
