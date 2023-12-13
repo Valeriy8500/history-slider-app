b691d2f7ac7dca0314f1e358a7818deae2487864
Valeriy Brazhnik <valerijbrazhnik@yandex.ru>
2023-12-13 19:09:31 +0300

    feat: реализовал анимацию дат

--- src/components/slider/slider.tsx
+++ src/components/slider/slider.tsx
@@ -1,10 +1,38 @@
+import { Navigation } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import './slider.scss';
+import 'swiper/scss';
+import 'swiper/scss/navigation';
+
 import { centerX, centerY, numPoints, radius, slideInfo } from '../../constans/constans';
 import { AiOutlineRightCircle, AiOutlineLeftCircle } from "react-icons/ai";
+import './slider.scss';
+import { ReactElement, useEffect, useState } from 'react';
 
-export const Slider = () => {
+export const Slider = (): ReactElement => {
+  const [slideState, setSlideState] = useState(slideInfo[0]);
+  const [currStartDate, setCurrStartDate] = useState(Number(slideState.startDate));
+  const [currEndDate, setCurrEndDate] = useState(Number(slideState.endDate));
+
+  useEffect(() => {
+    if (currStartDate < Number(slideState.startDate)) {
+      currStartDate !== Number(slideState.startDate) && setTimeout(setCurrStartDate, 20, currStartDate + 1);
+    } else {
+      currStartDate !== Number(slideState.startDate) && setTimeout(setCurrStartDate, 20, currStartDate - 1);
+    }
+  }, [currStartDate, slideState]);
+
+  useEffect(() => {
+    if (currEndDate < Number(slideState.endDate)) {
+      currEndDate !== Number(slideState.endDate) && setTimeout(setCurrEndDate, 20, currEndDate + 1);
+    } else {
+      currEndDate !== Number(slideState.endDate) && setTimeout(setCurrEndDate, 20, currEndDate - 1);
+    }
+  }, [currEndDate, slideState]);
+
+  const onPointClick = (id: number) => {
+    const newSlide = slideInfo.filter(i => i.id === id)[0];
+    setSlideState(newSlide);
+  };
 
   const points = slideInfo.map((item, index) => {
     const angle = (index / numPoints) * 2 * Math.PI;
@@ -17,14 +45,23 @@ export const Slider = () => {
         className="point"
         style={{ left: `${x}px`, top: `${y}px` }}
         id={String(item.id)}
+        onClick={() => onPointClick(item.id)}
       ></div>
     );
   });
 
   return (
-    <div className='slider-container'>
+    <div className='main-content'>
       <div className='circle-container'>
-        <div className="circle">{points}</div>
+        <div className="circle">
+          {points}
+          <div
+            className="circle-date-container"
+          >
+            <span>{currStartDate}</span>
+            <span>{currEndDate}</span>
+          </div>
+        </div>
         <div className='line horizontal-line'></div>
         <div className='line vertical-line'></div>
       </div>
@@ -32,25 +69,37 @@ export const Slider = () => {
       <div className='switches-container'>
         <div className='slide-number'>06/06</div>
         <div className='buttons'>
-          <button className='btn-left'><AiOutlineLeftCircle className='btn-icon' /></button>
-          <button className='btn-right'><AiOutlineRightCircle className='btn-icon' /></button>
+          <button className='btn-left'>
+            <AiOutlineLeftCircle className='btn-icon' />
+          </button>
+          <button className='btn-right'>
+            <AiOutlineRightCircle className='btn-icon' />
+          </button>
         </div>
       </div>
 
-      <div className='slider'></div>
+      <div className='slider-container'>
+        <Swiper
+          modules={[Navigation]}
+          spaceBetween={50}
+          slidesPerView={3}
+          navigation
+          onSlideChange={() => console.log('slide change')}
+          className='slider'
+        >
+          {slideState.events.map((item, idx) => {
+            return (
+              <SwiperSlide key={idx}>
+                <div className='slide'>
+                  <div className='slide-date'>{item.date}</div>
+                  <div className='slide-info'>{item.event}</div>
+                </div>
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
+      </div>
     </div>
   );
 }
 
-{/* <Swiper
-  spaceBetween={50}
-  slidesPerView={3}
-  onSlideChange={() => console.log('slide change')}
-  onSwiper={(swiper: any) => console.log(swiper)}
->
-  <SwiperSlide>Slide 1</SwiperSlide>
-  <SwiperSlide>Slide 2</SwiperSlide>
-  <SwiperSlide>Slide 3</SwiperSlide>
-  <SwiperSlide>Slide 4</SwiperSlide>
-  ...
-</Swiper> */}
